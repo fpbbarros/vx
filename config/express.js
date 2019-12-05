ded057e7304598b2b627e17179353aedb19ac5de
Fábio Pereira Barros <fpbbarros@gmail.com>
2019-12-05 16:46:28 -0300

    criacao do server

--- config/express.js
+++ config/express.js
@@ -3,10 +3,42 @@ const expressLoad = require('express-load');
 const bodyParser = require('body-parser');
 
 
-module.exports = function() {
+module.exports = function () {
 
   let app = express();
   const port = normalizePort(process.env.port || 3333);
 
+  //variaveis de ambiente
+
+  app.set('port', port);
+
+
   //
+
+  app.use(express.static('./public'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(require('method-override')());
+
+  return app;
+
 };
+
+
+// Normalize a port into a number, string, or false.
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('vxtel', ['ngRoute', 'ngResource'])
+  .config(function ($routeProvider) {
+    $routeProvider.when('/contatos')
+  });
+
