e07ad62f71b4069f81d202f380251b217151d9a9
Fábio Pereira Barros <fpbbarros@gmail.com>
2019-12-05 15:52:48 -0300

    início da aplicacao

---
+++ config/express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const expressLoad = require('express-load');
+const bodyParser = require('body-parser');
+
+
+module.exports = function() {
+
+  let app = express();
+  const port = normalizePort(process.env.port || 3333);
+
+  //
+};
